f20ffd7846a2a31566b966406a0bf124cd343c6e
Chris Kennedy <chriskennedy78@live.com>
2023-09-01 12:09:54 -0500

    updated header and footer styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,9 @@ import {
     NavbarToggler,
     Nav,
     NavItem,
+    Row,
+    Col,
+    Container,
 } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import CampoutLogo from "../app/assets/img/campout-logo.png";
@@ -14,56 +17,72 @@ const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     return (
         <Navbar dark color="primary" sticky="top" expand="md">
-            <NavbarBrand href="/" className="ms-5">
-                <img
-                    src={CampoutLogo}
-                    alt="CampOut logo"
-                    className="float-start"
-                    style={{ height: 100, width: 300 }}
-                />
-                <h6 className="mt-5">
-                    A secluded, natural, getaway for women. Home of the Virginia
-                    Women's Music Festival
-                </h6>
-            </NavbarBrand>
+            <Container>
+                <Row>
+                    <NavbarBrand href="/" className="ms-2">
+                        <img
+                            src={CampoutLogo}
+                            alt="CampOut logo"
+                            className="float-start"
+                            style={{ height: 100, width: 200 }}
+                        />
+                    </NavbarBrand>
+                    <h4>
+                        A secluded, natural, getaway for women. Home of the
+                        Virginia Women's Music Festival
+                    </h4>
+                </Row>
 
-            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
-            <Collapse isOpen={menuOpen} navbar>
-                <Nav className="ms-auto" navbar>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/">
-                            <i className="fa fa-home fa-lg" /> Home
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/Memberships">
-                            <i className="fa fa-list fa-lg" /> Memberships
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/cabinReservations">
-                            <i className="fa fa-solid fa-calendar fa-lg" />{" "}
-                            Cabin Reservations
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/tickets">
-                            <i className="fa fa-solid fa-ticket fa-lg" />{" "}
-                            Tickets
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/about">
-                            <i className="fa fa-info fa-lg" /> About
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/contact">
-                            <i className="fa fa-address-card fa-lg" /> Contact
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-            </Collapse>
+                <Row>
+                    <Col>
+                        <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+                        <Collapse isOpen={menuOpen} navbar>
+                            <Nav className="ms-auto" navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/">
+                                        <i className="fa fa-home fa-lg" /> Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink
+                                        className="nav-link"
+                                        to="/Memberships"
+                                    >
+                                        <i className="fa fa-list fa-lg" />{" "}
+                                        Memberships
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink
+                                        className="nav-link"
+                                        to="/cabinReservations"
+                                    >
+                                        <i className="fa fa-solid fa-calendar fa-lg" />{" "}
+                                        Cabin Reservations
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/tickets">
+                                        <i className="fa fa-solid fa-ticket fa-lg" />{" "}
+                                        Tickets
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/about">
+                                        <i className="fa fa-info fa-lg" /> About
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contact">
+                                        <i className="fa fa-address-card fa-lg" />{" "}
+                                        Contact
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Col>
+                </Row>
+            </Container>
         </Navbar>
     );
 };
